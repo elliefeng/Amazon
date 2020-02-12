e02f3ab1ee874437c6c46662fdc7f6befbf594c1
elliefeng <54426729+elliefeng@users.noreply.github.com>
2020-02-12 12:03:14 -0800

    Create Electronics.java

---
+++ Electronics.java
@@ -0,0 +1,38 @@
+public class Electronics extends Product
+{
+	
+	double myPrice;
+	String myBrand;
+	String myModel;
+	String myType;
+	
+	public Electronics(double price, String brand, String model, String type)
+	{
+		myPrice = price;
+		myBrand = brand;
+		myModel = model;
+		myType = type;	
+	}
+	
+	public double getPrice()
+	{
+		return myPrice;
+	}
+	
+	public String getBrand()
+	{
+		return myBrand;
+	}
+	
+	public String getModel()
+	{
+		return myModel;
+	}
+	
+	public String getType()
+	{
+		return myType;
+	}
+
+	
+}

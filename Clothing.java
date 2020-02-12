c5fb443784c64aa4a23acf25a9d80e6d0981ca80
elliefeng <54426729+elliefeng@users.noreply.github.com>
2020-02-12 12:02:57 -0800

    Create Clothing.java

---
+++ Clothing.java
@@ -0,0 +1,24 @@
+public  class Clothing extends Product {
+	double myPrice;
+	String mySize;
+	String myColor;
+	String myType;
+	public Clothing (double price, String size, String color, String type) {
+		myPrice = price;
+		mySize = size;
+		myColor = color;
+		myType = type;
+	}
+	public double getPrice() {
+		return myPrice;
+	}
+	public String getSize() {
+		return mySize;
+	}
+	public String getColor() {
+		return myColor;
+	}
+	public String getType() {
+		return myType;
+	}
+}

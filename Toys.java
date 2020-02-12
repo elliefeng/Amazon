610e4bb30411e2a092325a539d99323503320931
elliefeng <54426729+elliefeng@users.noreply.github.com>
2020-02-12 12:04:30 -0800

    Create Toys.java

---
+++ Toys.java
@@ -0,0 +1,33 @@
+
+public class Toys extends Product {
+
+	double myPrice;
+	String myBrand;
+	String myModel;
+	String myType;
+	
+	public Toys(double price, String brand, String model, String type) {
+		myPrice = price;
+		myBrand = brand;
+		myModel = model;
+		myType = type;
+	}
+	
+	public double getPrice() {
+		return myPrice;
+	}
+	
+	public String getBrand() {
+		return myBrand;
+	}
+	
+	public String getModel() {
+		return myModel;
+	}
+	
+	public String getType() {
+		return myType;
+	}
+
+	
+}

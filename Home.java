d04763e64f0a564e802861813c6f1ae565e2b917
elliefeng <54426729+elliefeng@users.noreply.github.com>
2020-02-12 12:03:31 -0800

    Create Home.java

---
+++ Home.java
@@ -0,0 +1,36 @@
+public class Home extends Product{
+	
+	double myPrice;
+	String myMaterial;
+	String myDimensions;
+	String myType;
+	
+	public Home(double price, String material, String dimensions, String type)
+	{
+		myPrice = price;
+		myMaterial = material;
+		myDimensions = dimensions;
+		myType = type;		
+	}
+	
+	public double getPrice()
+	{
+		return myPrice;
+	}
+	
+	public String getMaterials()
+	{
+		return myMaterial;
+	}
+	
+	public String getDimensions()
+	{
+		return myDimensions;
+	}
+	
+	public String getType()
+	{
+		return myType;
+	}
+}
+

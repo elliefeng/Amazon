a9bb7c31c8d66f4fdf7ef0b1db1dcc306a92d2ce
skylerwu <54692428+skylerwu@users.noreply.github.com>
2020-02-26 18:48:11 -0800

    Josh+Skyler Joint-Commit
    Fixed ArrayLists to include quantity, connected to UI.

--- src/ProductLists.java
+++ src/ProductLists.java
@@ -11,26 +11,42 @@ public class ProductLists {
 	shoes, 30.99 black
 	pants, 25.99 green */
 	
+	/*
+	 price brand model type
+	 */
+	
 	
 	public static void main(String[] args) {
 		addClothingProducts();
 		//System.out.println(listOfClothingProducts);
 		
 		for(int i=0;i<listOfClothingProducts.size();i++) {
-		System.out.println(listOfClothingProducts.get(i).myType);
+		System.out.println(listOfClothingProducts.get(i).myColor);
 		}
 		
+		//for(int x=0; x<listOfElectronicProducts.size(); x++);{
+			//System.out.println(listOfElectronicProducts.get(x).myColor);
+			
+		//}
 	}
 	
 	public static void addClothingProducts() {
-		Clothing shirt = new Clothing(19.99,"teal","shirt");
-		Clothing shoes = new Clothing(30.99,"black","shoes");
-		Clothing pants = new Clothing(25.99,"green","pants");
+		Clothing shirt = new Clothing(19.99,"teal","shirt",100);
+		Clothing shoes = new Clothing(30.99,"black","shoes",50);
+		Clothing pants = new Clothing(25.99,"green","pants",25);
 		listOfClothingProducts.add(shirt);
 		listOfClothingProducts.add(shoes);
 		listOfClothingProducts.add(pants);
 	}
 	
 	
+	public static void addElectronicProducts() {
+		Electronics phone = new Electronics(112.95, "Nokia", "The brick phone", "phone");
+		Electronics tv = new Electronics(699.99, "Samsung", "420 inch 8k smart tv", "television");
+		Electronics laptop =  new Electronics(399.99, "Apple", "totaly litty quantum computer 3000", "laptop");
+		listOfElectronicProducts.add(phone);
+		listOfElectronicProducts.add(tv);
+		listOfElectronicProducts.add(laptop);
+	}
 	
 }

f434d33bb881531680c7b9afc1d4d5ce40980e21
skylerwu <54692428+skylerwu@users.noreply.github.com>
2020-02-27 12:25:23 -0800

    Josh+Skyler Joint Commit

--- src/Electronics_UI.java
+++ src/Electronics_UI.java
@@ -2,27 +2,28 @@
 
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 
 public class Electronics_UI extends Product_UI{
-
-	/**
-	 * 
-	 */
-	public static String name;
-	public static double price;
-	public static String brand;
+ public static int i = 0;
+ 
+ public static JLabel lblImage = new JLabel("");
+	
 	private static final long serialVersionUID = -5865436019373463725L;
 
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Electronics_UI frame = new Electronics_UI(name, price, brand);
+					ProductLists.addElectronicProducts();
+					Electronics_UI frame = new Electronics_UI(i);
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -31,8 +32,9 @@ public class Electronics_UI extends Product_UI{
 		});
 	}
 	
-	public Electronics_UI(String name, double price,String brand) {
-		super(name, price, brand);
+	public Electronics_UI(int test) {
+		super();
+		i = test;
 		lblAmazon.setVisible(false);
 		/*this.name = name;
 		this.price = price;
@@ -50,17 +52,17 @@ public class Electronics_UI extends Product_UI{
 		getContentPane().add(lblImage);
 		
 		JLabel txtrProduct = new JLabel();
-		txtrProduct.setText("Product: " + name);
+		txtrProduct.setText("Product: " + ProductLists.listOfElectronicProducts.get(i).myType);
 		txtrProduct.setBounds(28, 181, 132, 32);
 		getContentPane().add(txtrProduct);
 		
 		JLabel txtrPrice = new JLabel();
-		txtrPrice.setText("Price: $" + price);
+		txtrPrice.setText("Price: $" + ProductLists.listOfElectronicProducts.get(i).myPrice);
 		txtrPrice.setBounds(193, 35, 132, 32);
 		getContentPane().add(txtrPrice);
 		
 		JLabel txtrAttribute = new JLabel();
-		txtrAttribute.setText("Brand: " + brand);
+		txtrAttribute.setText("Brand: " + ProductLists.listOfElectronicProducts.get(i).myBrand);
 		txtrAttribute.setBounds(193, 66, 132, 32);
 		getContentPane().add(txtrAttribute);
 		
@@ -78,5 +80,47 @@ public class Electronics_UI extends Product_UI{
 		lblNewLabel.setBounds(203, 130, 54, 16);
 		getContentPane().add(lblNewLabel);
 		
+		JLabel lblRemainingStock = new JLabel("Remaining Stock: " + ProductLists.listOfClothingProducts.get(i).myQuantity);
+		lblRemainingStock.setOpaque(true);
+		lblRemainingStock.setBounds(193, 96, 212, 28);
+		getContentPane().add(lblRemainingStock);
+		
+		JButton btnNext = new JButton("Next");
+		btnNext.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				i = (i+1)%3;
+				System.out.println(i);
+				txtrProduct.setText("Product: " + ProductLists.listOfElectronicProducts.get(i).myType);
+				txtrPrice.setText("Price: $" + ProductLists.listOfElectronicProducts.get(i).myPrice);
+				txtrAttribute.setText("Brand: " + ProductLists.listOfElectronicProducts.get(i).myBrand);
+				lblRemainingStock.setText("Remaining Stock: " + ProductLists.listOfElectronicProducts.get(i).myQuantity);
+				lblImage.setIcon(new ImageIcon(ProductLists.listOfElectronicProducts.get(i).myImage));
+			}
+		});
+		btnNext.setBounds(321, 40, 105, 23);
+		getContentPane().add(btnNext);
+	
+		
+		JButton btnPrevious = new JButton("Previous");
+		btnPrevious.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				i = (i+2)%3;
+				System.out.println(i);
+				txtrProduct.setText("Product: " + ProductLists.listOfElectronicProducts.get(i).myType);
+				txtrPrice.setText("Price: $" + ProductLists.listOfElectronicProducts.get(i).myPrice);
+				txtrAttribute.setText("Brand: " + ProductLists.listOfElectronicProducts.get(i).myBrand);
+				lblRemainingStock.setText("Remaining Stock: " + ProductLists.listOfElectronicProducts.get(i).myQuantity);
+				lblImage.setIcon(new ImageIcon(ProductLists.listOfElectronicProducts.get(i).myImage));
+				
+			}
+			
+		});
+		btnPrevious.setBounds(335, 71, 91, 23);
+		getContentPane().add(btnPrevious);
+		
 	}
 }
+			
+	
+
+	

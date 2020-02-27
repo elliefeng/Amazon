98dc5d5bccc3be253b3772369afa85561a17fdd3
skylerwu <54692428+skylerwu@users.noreply.github.com>
2020-02-26 18:45:55 -0800

    Josh+Skyler Joint-Commit
    Fixed quantity system.

--- src/Clothing.java
+++ src/Clothing.java
@@ -1,18 +1,20 @@
 package src;
 
-
+import src.Product;
 
 public class Clothing extends Product {
 	double myPrice;
 	String mySize;
 	String myColor;
 	String myType;
+	int myQuantity;
 	int commit;
 	
-	public Clothing (double price, String color, String type) {
+	public Clothing (double price, String color, String type, int quantity) {
 		myPrice = price;
 		myColor = color;
 		myType = type;
+		myQuantity = quantity;
 	}
 	
 	public double getPrice() {
@@ -27,6 +29,9 @@ public class Clothing extends Product {
 	}
 	public String getType() {
 		return myType;
+	}
+	public String getQuantity() {
+		return Integer.toString(myQuantity);
 	}
 	public String toString() {
 		return (myType + ", " + myColor + ": " + "$" + myPrice);

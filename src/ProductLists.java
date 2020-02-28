b9892a122dd134d6c25e33d6bfed2b45fbfdd6d9
skylerwu <54692428+skylerwu@users.noreply.github.com>
2020-02-28 11:43:18 -0800

    Josh, Skyler, Andrew joint-commit.

--- src/ProductLists.java
+++ src/ProductLists.java
@@ -6,6 +6,8 @@ public class ProductLists {
 
 	public static ArrayList<Clothing> listOfClothingProducts = new ArrayList<Clothing>();
 	public static ArrayList<Electronics> listOfElectronicProducts =  new ArrayList<Electronics>();
+	public static ArrayList<Home> listOfHomeProducts = new ArrayList<Home>();
+	public static ArrayList<Toys> listOfToyProducts = new ArrayList<Toys>();
 	
 	/*Clothing 
 	shirt, 19.99 teal 
@@ -32,9 +34,9 @@ public class ProductLists {
 	}
 	
 	public static void addClothingProducts() {
-		Clothing shirt = new Clothing(19.99,"teal","shirt",100,"src/nike.jpg");
-		Clothing shoes = new Clothing(30.99,"black","shoes",50,"src/shoes.jpg");
-		Clothing pants = new Clothing(25.99,"green","pants",25,"src/pants.jpg");
+		Clothing shirt = new Clothing(19.99,"Teal","Shirt",100,"src/nike.jpg");
+		Clothing shoes = new Clothing(30.99,"Black","Shoes",50,"src/shoes.jpg");
+		Clothing pants = new Clothing(25.99,"Green","Pants",25,"src/pants.jpg");
 		listOfClothingProducts.add(shirt);
 		listOfClothingProducts.add(shoes);
 		listOfClothingProducts.add(pants);
@@ -42,12 +44,28 @@ public class ProductLists {
 	
 	
 	public static void addElectronicProducts() {
-		Electronics phone = new Electronics(112.95, "Nokia", "phone", 420, "src/nokia.jpg");
-		Electronics tv = new Electronics(699.99, "Samsung","television", 69, "src/tv.jpg");
-		Electronics laptop =  new Electronics(399.99, "Apple", "laptop", 1234, "src/computer.jpg");
+		Electronics phone = new Electronics(112.95, "Nokia", "Phone", 420, "src/nokia.jpg");
+		Electronics tv = new Electronics(699.99, "Samsung","Television", 69, "src/tv.jpg");
+		Electronics laptop =  new Electronics(399.99, "Apple", "Laptop", 1234, "src/computer.jpg");
 		listOfElectronicProducts.add(phone);
 		listOfElectronicProducts.add(tv);
 		listOfElectronicProducts.add(laptop);
+	}
+	
+	public static void addHomeProducts() {
+		Home table = new Home(24.99, "Titanium","Table", 345, "src/table.jpg");
+		Home sofa = new Home(700.00, "Cashmere", "Sofa", 77, "src/couch.jpg");
+		listOfHomeProducts.add(table);
+		listOfHomeProducts.add(sofa);
+	}
+	
+	public static void addToyProducts() {
+		Toys rubberChicken = new Toys(27.99, "Mattel", "Rubber Chicken", 600, "src/rubberchicken.jpg");
+		Toys stuffedAnimal = new Toys(4.95, "Hasbro", "Stuffed Animal", 9000, "src/stuffedanimal.jpg");
+		Toys lego = new Toys(274.99, "LEGO Corporation", "Ninjago Ultra Dragon", 678, "src/lego.jpg");
+		listOfToyProducts.add(rubberChicken);
+		listOfToyProducts.add(stuffedAnimal);
+		listOfToyProducts.add(lego);
 	}
 	
 }

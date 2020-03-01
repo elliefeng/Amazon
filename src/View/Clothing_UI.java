e7ef1655a4066206c051f9d8cffdcbb6e2e0f8e3
John Mortensen <jmortensen@powayusd.com>
2020-03-01 09:29:05 -0800

    noop

--- src/View/Clothing_UI.java
+++ src/View/Clothing_UI.java
@@ -18,8 +18,8 @@ import javax.swing.JButton;
 import javax.swing.JComboBox;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
+//import javax.swing.JTextArea;
+//import javax.swing.JTextField;
 
 import Control.ProductList;
 import Model.Clothing;
